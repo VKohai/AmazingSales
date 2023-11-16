5bbc90b9e93a1a7588b45e41da5de2ac93fb13c2
Kohaikaa <cherry.designwork@gmail.com>
2023-11-16 18:31:42 +0300

    feature: BadgeChat

---
+++ src/AmazingSales.Domain/Entities/BadgeChat/Chat.cs
@@ -0,0 +1,27 @@
+using AmazingSales.Domain.Common;
+using AmazingSales.Domain.Entities.BadgeAnnouncement;
+using AmazingSales.Domain.Entities.BadgeProfile;
+
+namespace AmazingSales.Domain.Entities.BadgeChat
+{
+    public sealed class Chat : BaseEntity
+    {
+        public Profile Seller { get; private set; }
+        public Profile Customer { get; private set; }
+        public Announcement Announcement { get; private set; }
+        public DateTime Created { get; private set; }
+
+        public Chat(
+            long id,
+            Profile seller,
+            Profile customer,
+            Announcement announcement
+        ) : base(id)
+        {
+            Seller = seller;
+            Customer = customer;
+            Announcement = announcement;
+            Created = DateTime.Now;
+        }
+    }
+}
---
+++ src/AmazingSales.Domain/Entities/BadgeChat/Message.cs
@@ -0,0 +1,29 @@
+using AmazingSales.Domain.Common;
+using AmazingSales.Domain.Entities.BadgeProfile;
+
+namespace AmazingSales.Domain.Entities.BadgeChat
+{
+    public sealed class Message : BaseEntity
+    {
+        public Chat Chat { get; private set; }
+        public Profile Sender { get; private set; }
+        public string MessageText { get; private set; }
+        public string AttachmentUrl { get; private set; }
+        public DateTime Created { get; private set; }
+
+        public Message(
+            long id,
+            Chat chat,
+            Profile sender,
+            string? messageText,
+            string? attachmentUrl
+        ) : base(id)
+        {
+            Chat = chat;
+            Sender = sender;
+            MessageText = messageText ?? string.Empty;
+            AttachmentUrl = attachmentUrl ?? string.Empty;
+            Created = DateTime.Now;
+        }
+    }
+}
