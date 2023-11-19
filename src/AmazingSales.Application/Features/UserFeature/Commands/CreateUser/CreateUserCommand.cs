d9abf74555722b23e5cac1c2c24e4da79962a47e
Kohaikaa <cherry.designwork@gmail.com>
2023-11-19 11:28:54 +0300

    feature: CreateUserCommand

---
+++ src/AmazingSales.Application/Features/UserFeature/Commands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,44 @@
+using MediatR;
+using AmazingSales.Domain.Entities;
+using AmazingSales.Application.Interfaces.Repositories;
+
+namespace AmazingSales.Application.Features.UserFeature.Commands.CreateUser
+{
+    public record CreateUserCommand : IRequest<long>
+    {
+        public string? Name { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+    }
+
+    internal class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, long>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CreateUserCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<long> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+            // Creating a new user entity and mapping the properties of the command
+            // object with the player entity.
+            var user = new User(request.Name!, request.Email!, request.Phone);
+
+            // Adding the user in the database using the generic AddAsync method.
+            var createdUser = await _unitOfWork.Repository<User, long>().AddAsync(user);
+
+            // Adding the UserCreatedEvent event in the domain events collection
+            // available in BaseEntity class.
+            user.AddDomainEvent(new UserCreatedEvent(user));
+
+            // Saving changes of adding the user
+            await _unitOfWork.Save(cancellationToken);
+
+            // Return id of the new user
+            return createdUser.Id;
+        }
+    }
+
+}
---
+++ src/AmazingSales.Application/Features/UserFeature/Commands/CreateUser/UserCreatedEvent.cs
@@ -0,0 +1,16 @@
+using AmazingSales.Domain.Common;
+using AmazingSales.Domain.Entities;
+
+namespace AmazingSales.Application.Features.UserFeature.Commands.CreateUser
+{
+    public class UserCreatedEvent : BaseEvent
+    {
+        public User User { get; }
+
+        public UserCreatedEvent(User user)
+        {
+            User = user;
+        }
+
+    }
+}
