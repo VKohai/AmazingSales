1b693ee38014c82dea93bedefe93342bc6dc0203
Kohaikaa <cherry.designwork@gmail.com>
2023-11-24 22:28:59 +0300

    deleted: Features folder is deleted

--- src/AmazingSales.Application/Features/UserFeature/Queries/GetUsersWithPagination/GetUsersWithPaginationDto.cs
+++
@@ -1,9 +0,0 @@
-namespace AmazingSales.Application.Features.UserFeature.Queries
-{
-    public class GetUsersWithPaginationDto
-    {
-        public string? Name { get; set; }
-        public string? Phone { get; set; }
-        public string? Email { get; set; }
-    }
-}
